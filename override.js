44619563ff168a8e4af48fd5ea8393ae6add2c4d
Your Name <you@example.com>
2017-08-13 08:48:02 +0530

    removed unnecessary whitespaces

--- override.js
+++ override.js
@@ -101,11 +101,7 @@ $(function(){
     
     function fetchQuotesAPI(){
 
-
         var url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
-
-        
-        
         $.getJSON(url, function(data) {
 
 
@@ -115,15 +111,8 @@ $(function(){
             localStorage.setItem('currentAuthor',currentAuthor);
             $('#quoteblock').text(currentQuote);
             $('#author').text(currentAuthor);
-
                 
             });
-
-
- 
-   
-
-
 
     };
 
